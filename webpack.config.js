de2aabcf3da041dbfb54fb0e245539ecf010216e
Hon Lam <hon@junglescout.com>
2018-10-04 00:29:30 -0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+
+  // IMPORTANT: Sort keys alphabetically
+
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  entry: './src/index.js',
+  module: {
+    rules: [
+      { 
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
